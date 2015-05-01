47b798173ea7eafb7442eeb76a9ad08544005de8
Tim Paul <tim.paul@digital.cabinet-office.gov.uk>
2015-05-01 15:04:07 +0100

    Draft new style form fields

--- app/routes.js
+++ app/routes.js
@@ -11,5 +11,32 @@ module.exports = {
 
     // add your routes here
 
+
+    // Check if new user
+    app.get('/examples/account-management/new-user-check', function (req, res) {
+
+      switch (req.query['new-user']){
+      	case 'no'	: 	res.redirect('examples/account-management/create-account');
+      	case 'yes'	:  	res.redirect('examples/account-management/sign-in');
+      	default		:   res.render('examples/account-management/new-user-check');
+      }
+
+    });
+
+
+    // Create account using email, mobile or username
+    app.get('/examples/account-management/create-account-2', function (req, res) {
+
+      switch (req.query['sign-in']){
+        case 'email'  :   res.redirect('examples/account-management/create-account-email');
+        case 'mobile' :   res.redirect('examples/account-management/create-account-mobile');
+        case 'neither':   res.redirect('examples/account-management/create-account-username');
+        default       :   res.render('examples/account-management/create-account-2');
+      }
+
+    });
+
+
+
   }
 };
--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use('/public', express.static(__dirname + '/govuk_modules/govuk_template/assets'));
 app.use('/public', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit'));
 
+app.use('/icons', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit/images'));
+
 app.use(express.favicon(path.join(__dirname, 'govuk_modules', 'govuk_template', 'assets', 'images','favicon.ico'))); 
 
 
